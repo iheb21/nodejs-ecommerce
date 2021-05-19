09f460b3f485ccabd0ecf1def339df2f46a5c836
Michael <you@example.com>
2021-05-19 23:53:39 +0200

    Initial commit

---
+++ routes/product_route.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var conn = require('../database');
+var mysql = require('mysql');
+var bcrypt = require('bcrypt');
+
+// TODO : A reprendre : Ajout d'une route paramétrée pour récupérer les informations du produit
+router.get('/product/:id', function (req, res, next) {
+    let query = 'SELECT * FROM products WHERE id = ? LIMIT 1';
+    conn.query(query, [req.params.id],(err, rows) => {
+        if (err) {
+            req.flash('error', err);
+        } else {
+            const product = rows.length > 0 ? rows[0] : null;
+            if (product) {
+                query = 'SELECT * FROM reviews WHERE product_id = ?';
+                conn.query(query, [req.params.id], (err, rows) => {
+                    res.render('product', { product, reviews: rows });
+                });
+            } else {
+                res.redirect('/shop');
+            }
+        }
+    });
+});
+module.exports = router;
---
+++ routes/shop_route.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var router = express.Router();
+var conn = require('../database');
+var mysql = require('mysql');
+var bcrypt = require('bcrypt');
+
+router.get('/shop', function (req, res, next) {
+    var produit = '';
+    var categorie = '';
+    if (req.query.categorie === undefined) {
+        conn.query('SELECT * FROM products ORDER BY id desc', function (err, rows) {
+
+            if (err) {
+                req.flash('error', err);
+
+            } else {
+                produit = rows;
+                conn.query('SELECT * FROM categorie ORDER BY id desc', function (err, rows) {
+
+                    if (err) {
+                        req.flash('error', err);
+
+
+                    } else {
+                        categorie = rows;
+                        res.render('shop', { data: produit, categorie });
+                    }
+                });
+            }
+
+        });
+    } else {
+        conn.query('SELECT * FROM products where categorie=' + req.query.categorie + ' ORDER BY id desc', function (err, rows) {
+
+            if (err) {
+                console.log('error', err);
+
+            } else {
+                produit = rows;
+                conn.query('SELECT * FROM categorie ORDER BY id desc', function (err, rows) {
+
+                    if (err) {
+                        req.flash('error', err);
+
+                    } else {
+                        categorie = rows;
+                        res.render('shop', { data: produit, categorie });
+                    }
+                });
+            }
+
+        });
+    }
+});
+module.exports = router;
